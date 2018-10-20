f748cb74608480af744251623aaf89df2168e7e2
Ricardo Rebello Sierra <ricardo@sierratecnologia.com.br>
2018-10-20 16:43:01 -0300

    Mudando texto para tópicos e articos classificados como não interessante

--- resources/views/topics/partials/ribbon.blade.php
+++ resources/views/topics/partials/ribbon.blade.php
@@ -10,11 +10,7 @@
 @if ($topic->order == -1)
   <div class="ribbon">
       <div class="ribbon-anchored">
-            @if ($topic->category_id === 1)
-                <i class="fa fa-anchor"></i> Este post foi <a href="https://h3sotospeak.com/topics/2802">Afundar</a>，Por favor siga <a href="https://h3sotospeak.com/topics/817/hoppe-brasil-recruitment-post-specification">Especificação de lançamento de recrutamento</a>Modificado, alterado @ admin cancelado naufrágio.
-            @else
-                <i class="fa fa-anchor"></i> Esta etiqueta afundou, por favor verifique <a href="https://h3sotospeak.com/topics/2802">Instruções afundando</a> Modificado, alterado @@ admin cancelado naufrágio.
-            @endif
+            <i class="fa fa-anchor"></i> Este tópico foi classificado como não relevante. Por favor verifique nossas regras para publicação.
       </div>
   </div>
 @endif
